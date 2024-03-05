a707bd95ee4b7d00ecb82ff173afdea1e6ccb69f
islam302 <islambadran39@gmail.com>
2024-03-05 15:35:45 +0200

    try ro deploy

--- The_Nurse/settings.py
+++ The_Nurse/settings.py
@@ -22,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^pnd7&$mb58mlxww)q-6*4^7+=+cgn$f1oe+=iwl(6(_j-=2bi'
+# SECRET_KEY = 'django-insecure-^pnd7&$mb58mlxww)q-6*4^7+=+cgn$f1oe+=iwl(6(_j-=2bi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 
 # Application definition
